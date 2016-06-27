7d2ebf7f6ed8f362881371a2412cd275a0ae3506
ice <ice@lab05m44.nrc.ice.ufjf.br>
2016-06-27 17:42:43 -0300

    Modelo

---
+++ HigieneEPerfumaria.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class HigieneEPerfumaria here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HigieneEPerfumaria extends Produto
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class HigieneEPerfumaria
+     */
+    public HigieneEPerfumaria()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
